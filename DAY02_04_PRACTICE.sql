be363826ca8e39931cf06c070b780eb59a9f8cdd
greyBrown <greybrown4645@gmail.com>
2023-12-28 17:43:22 +0900

    rowid

--- DAY02_04_PRACTICE.sql
+++ DAY02_04_PRACTICE.sql
@@ -2,6 +2,11 @@
 SELECT DISTINCT DEPARTMENT_ID
 FROM EMPLOYEES ;
 
+SELECT * from EMPLOYEES where rowid in (select max(rowid) from EMPLOYEES group by DEPARTMENT_ID); 
+-- 중복된 DEPARTMENT 값을 제거하고 남은 컬럼들을 모두 보여주는 명령문! 다른 칼럼들은 안보여주나? 싶어서 구글링해서 해봤다
+-- 요컨대 DEPARTMENT_ID가 중복이 있다면...ROWID값이 가장 높은 행 하나만 제외하고 나머지는 SELECT에서 제외되는데, ROWID값이 가장 높다고해서 가장 보스일거란 확증은 없음 ㅋㅋㅋ
+-- ROWID 값은 변경되기 쉬운 값이라고 한다. 테이블 수정/변경하면서 바뀌기도 하고...명령문의 세세한 의미를 명확히 모르겠으니 티스토리 마지막에 이걸 분석해보도록 합시다.
+
 -- 2. 사원 테이블에서 사원번호가 150인 사원의 정보 조회하기
 SELECT *
 FROM EMPLOYEES
